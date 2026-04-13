a9f508418cc995c8d2181fa8b692f03ccd85e37a
Emiliano Montes <101071655+louieliet@users.noreply.github.com>
2026-04-13 09:22:44 -0600

    Initial Commit

---
+++ tictactoe/tictactoeApp.swift
@@ -0,0 +1,17 @@
+//
+//  tictactoeApp.swift
+//  tictactoe
+//
+//  Created by Emiliano Montes Gómez on 13/04/26.
+//
+
+import SwiftUI
+
+@main
+struct tictactoeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
